e592cf5a90d6992c6d2b9ac570e3df8336e8def2
stevenhuang2020 <stevenhuang1017@gmail.com>
2020-07-02 16:16:13 +1200

    add manifoldLearning non-linear dimensionality reduction

--- manifoldLearning.py
+++ manifoldLearning.py
@@ -77,28 +77,19 @@ def manifoldLearningTransform(X,n_components=2, n_neighbors=10,name='SE',eigen_s
     assert(0)
             
 def plotManifold(X,labels,n_components=2, n_neighbors=10):
-    LLE = partial(manifold.LocallyLinearEmbedding, n_neighbors, n_components, eigen_solver='auto')
-
-    methods = OrderedDict()
-    methods['LLE'] = LLE(method='standard')
-    methods['LTSA'] = LLE(method='ltsa')
-    #methods['Hessian LLE'] = LLE(method='hessian')
-    #methods['Modified LLE'] = LLE(method='modified')
-    methods['Isomap'] = manifold.Isomap(n_neighbors, n_components)
-    methods['MDS'] = manifold.MDS(n_components, max_iter=100, n_init=1)
-    methods['SE'] = manifold.SpectralEmbedding(n_components=n_components,  n_neighbors=n_neighbors)
-    methods['t-SNE'] = manifold.TSNE(n_components=n_components, init='pca', random_state=0)
-
-    # Plot results
-    for i, (label, method) in enumerate(methods.items()):
+    names=['LLE','LTSA','Hessian LLE','Modified LLE','Isomap','MDS','SE','t-SNE']
+    for name in names:
         t0 = time()
-        Y = method.fit_transform(X)
+        if name == 'Hessian LLE' or name == 'Modified LLE' or name == 'LTSA':
+            Y = manifoldLearningTransform(X,name=name,eigen_solver='dense')
+        else:
+            Y = manifoldLearningTransform(X,name=name)
+            
         t1 = time()
-        print("%s: %.2g sec" % (label, t1 - t0))
-        #ax = fig.add_subplot(2, 5, 2 + i + (i > 3))
+        print("%s: %.2g sec" % (name, t1 - t0))
         ax = plt.subplot(1,1,1)
         ax.scatter(Y[:, 0], Y[:, 1], c=labels, cmap=plt.cm.Spectral)
-        ax.set_title("%s (%.2g sec)" % (label, t1 - t0))
+        ax.set_title("%s (%.2g sec)" % (name, t1 - t0))
         ax.xaxis.set_major_formatter(NullFormatter())
         ax.yaxis.set_major_formatter(NullFormatter())
         ax.axis('tight')
