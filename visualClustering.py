c42facf9e547dd0494c8aee21098f8ffa6d0aad8
stevenhuang2020 <stevenhuang1017@gmail.com>
2020-07-02 14:35:28 +1200

    add manifoldLearning non-linear dimensionality reduction

--- visualClustering.py
+++ visualClustering.py
@@ -6,16 +6,35 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler,MinMaxScaler
 from sklearn.metrics import silhouette_score
 from tsne import tsne
+from manifoldLearning import manifoldLearningTransform,plotManifold
 
 def visualClusterResult(data, labels, bestK, title):
+    #return plotManifold(data,labels)
     print('data.shape=',data.shape,'labels.shape=',labels.shape,'bestK=',bestK)
-    if 1:
+    if 0:
         method='PCA'
         Y = PCA(n_components=2).fit_transform(data)
-    else:
+    elif 0:
         #Y = tsne(data, 2, 10, 50,max_iter=500)
         Y = TSNE(n_components=2, verbose=0, perplexity=50, n_iter=1000).fit_transform(data)#scikit-learn
         method='TSNE'
+    else:
+        name = 'LLE'
+        name = 'LTSA'
+        name = 'Hessian LLE'
+        name = 'Modified LLE'
+        name = 'Isomap'
+        name = 'MDS'
+        name = 'SE'
+        name = 't-SNE'
+        
+        if name == 'Hessian LLE' or name == 'Modified LLE' or name == 'LTSA':
+            Y = manifoldLearningTransform(data,name=name,eigen_solver='dense')
+        else:
+            Y = manifoldLearningTransform(data,name=name)
+        
+        method='manifold' + '_' + name
+        
     title = title + '_' + method + '_k_'+str(bestK)
     plt.title(title)
     plt.scatter(Y[:, 0], Y[:, 1], 20, labels) #marker='s',edgecolor='black'
